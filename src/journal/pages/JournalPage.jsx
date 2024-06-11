6d78ef4c10b849f997a5016c627f72f8a889c0ea
Lennin Ibarra <ing.lenninibarra@gmail.com>
2024-06-11 09:56:05 -0500

    feature/installMUI: se instala material ui y se configura el AppTheme

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,3 +1,10 @@
+import { MailOutline } from "@mui/icons-material";
+import Typography from "@mui/material/Typography";
 export const JournalPage = () => {
-  return <div>journalPage</div>;
+  return (
+    <>
+      <Typography variant="h1">Journal Page</Typography>
+      <MailOutline />
+    </>
+  );
 };
